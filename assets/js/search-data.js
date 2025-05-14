9ac9c02dcb0a4ce5f49dbdd5e3a7aa3e17d7f77a
segamej <garcia06@umd.edu>
2025-05-14 16:26:01 +0000

    Deploying to gh-pages from @ segamej/segamej.github.io@1b7bca9f4ff8fc2d3858b7b4eaf2e8d9dffc04c8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,7 +445,7 @@ ninja.data = [{
         title: 'Bluesky',
         section: 'Socials',
         handler: () => {
-          window.open("segamej.bsky.social", "_blank");
+          window.open("https://bsky.app/profile/segamej.bsky.social", "_blank");
         },
       },{
         id: 'social-email',
